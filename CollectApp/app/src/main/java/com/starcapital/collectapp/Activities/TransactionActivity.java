a323a345ae22eda9ff002189aeb29b3fc74483d4
Stephen Obeng Takyi <stephenta@stlghana.com>
2019-05-22 17:59:57 +0000

    Functionality for Transaction Activity and Capture Signature Activity. DialogUtility class and Utility class was created. Dialogs functionality and Capture Signature functionality started.

--- CollectApp/app/src/main/java/com/starcapital/collectapp/Activities/TransactionActivity.java
+++ CollectApp/app/src/main/java/com/starcapital/collectapp/Activities/TransactionActivity.java
@@ -1,15 +1,84 @@
 package com.starcapital.collectapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.starcapital.collectapp.R;
+import com.starcapital.collectapp.Utilities.CaptureSignature;
+import com.starcapital.collectapp.Utilities.DialogUtility;
 
 public class TransactionActivity extends AppCompatActivity {
+
+    EditText editTextAccNum, editTextAccName, editTextTransType, editTextAmount, editTextOfficer, editTextBranch;
+    Button buttonCancel, buttonSubmit;
+    DialogUtility dialogUtility;
+    public static final int SIGNATURE_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
+
+        dialogUtility = new DialogUtility(TransactionActivity.this);
+        init();
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TransactionActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CaptureSignature.class);
+                startActivityForResult(intent, SIGNATURE_ACTIVITY);
+            }
+        });
+
+    }
+
+    private void init() {
+
+        editTextAccNum = findViewById(R.id.transaction_acc_number);
+        editTextAccName = findViewById(R.id.transaction_acc_name);
+        editTextTransType = findViewById(R.id.transaction_trans_type);
+        editTextAmount = findViewById(R.id.transaction_amount);
+        editTextOfficer = findViewById(R.id.transaction_officer_name);
+        editTextBranch = findViewById(R.id.transaction_branch);
+
+        buttonCancel = findViewById(R.id.transaction_button_cancel);
+        buttonSubmit = findViewById(R.id.transaction_button_submit);
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch(requestCode) {
+            case SIGNATURE_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+
+                    Bundle bundle = data.getExtras();
+                    String status  = bundle.getString("status");
+                    if(status.equalsIgnoreCase("done")){
+                        Toast toast = Toast.makeText(this, "Signature capture successful!", Toast.LENGTH_SHORT);
+                        toast.show();
+                        byte[] sign = bundle.getByteArray("encodedImage");
+                        Log.d("Bundle Breakdown", "  "+bundle.toString());
+                    }
+                }
+                break;
+        }
+
     }
 }
---
+++ CollectApp/app/src/main/java/com/starcapital/collectapp/Utilities/CaptureSignature.java
@@ -0,0 +1,339 @@
+package com.starcapital.collectapp.Utilities;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.starcapital.collectapp.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+public class CaptureSignature extends Activity {
+
+    LinearLayout mContent;
+    signature mSignature;
+    Button mClear, mGetSign, mCancel;
+    public static String tempDir;
+    public int count = 1;
+    public String current = null;
+    private Bitmap mBitmap;
+    View mView;
+    File mypath;
+
+    private String uniqueId;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.confirm_transaction_dialog);
+
+
+
+
+        tempDir = Environment.getExternalStorageDirectory() + "/" + getResources().getString(R.string.external_dir) + "/";
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir(getResources().getString(R.string.external_dir), Context.MODE_PRIVATE);
+
+        prepareDirectory();
+        uniqueId = getTodaysDate() + "_" + getCurrentTime() + "_" + Math.random();
+        current = uniqueId + ".png";
+        mypath = new File(directory, current);
+
+
+        mContent = findViewById(R.id.sign_linear_layout);
+        mSignature = new signature(this, null);
+        mSignature.setBackgroundColor(Color.WHITE);
+        mContent.addView(mSignature, LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+        mClear = findViewById(R.id.sign_button_clear);
+        mGetSign = findViewById(R.id.sign_button_confirm);
+        mGetSign.setEnabled(false);
+        mCancel = findViewById(R.id.sign_button_cancel);
+        mView = mContent;
+
+        mClear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v("log_tag", "Panel Cleared");
+                mSignature.clear();
+                mGetSign.setEnabled(false);
+            }
+        });
+
+        mGetSign.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(CaptureSignature.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(CaptureSignature.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CaptureSignature.this,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+                    Toast.makeText(CaptureSignature.this, "You need to have granted permission", Toast.LENGTH_SHORT).show();
+                }else{
+                    Log.v("log_tag", "Panel Saved");
+                    boolean error = captureSignature();
+                    if (!error) {
+                        mView.setDrawingCacheEnabled(true);
+                        mSignature.save(mView);
+                        Bundle b = new Bundle();
+                        b.putString("status", "done");
+                        Intent intent = new Intent();
+                        intent.putExtras(b);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                }
+            }
+        });
+
+
+
+
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.v("log_tag", "Panel Canceled");
+                Bundle b = new Bundle();
+                b.putString("status", "cancel");
+                Intent intent = new Intent();
+                intent.putExtras(b);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.w("GetSignature", "onDestory");
+        super.onDestroy();
+    }
+
+    private boolean captureSignature() {
+
+        boolean error = false;
+        String errorMessage = "";
+
+        if (error) {
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP, 105, 50);
+            toast.show();
+        }
+
+        return error;
+    }
+
+    private String getTodaysDate() {
+
+        final Calendar c = Calendar.getInstance();
+        int todaysDate = (c.get(Calendar.YEAR) * 10000) +
+                ((c.get(Calendar.MONTH) + 1) * 100) +
+                (c.get(Calendar.DAY_OF_MONTH));
+        Log.w("DATE:", String.valueOf(todaysDate));
+        return (String.valueOf(todaysDate));
+
+    }
+
+    private String getCurrentTime() {
+
+        final Calendar c = Calendar.getInstance();
+        int currentTime = (c.get(Calendar.HOUR_OF_DAY) * 10000) +
+                (c.get(Calendar.MINUTE) * 100) +
+                (c.get(Calendar.SECOND));
+        Log.w("TIME:", String.valueOf(currentTime));
+        return (String.valueOf(currentTime));
+
+    }
+
+
+    private boolean prepareDirectory() {
+        try {
+            if (makedirs()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Could not initiate File System.. Is Sdcard mounted properly?", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    private boolean makedirs() {
+        File tempdir = new File(tempDir);
+        if (!tempdir.exists())
+            tempdir.mkdirs();
+
+        if (tempdir.isDirectory()) {
+            File[] files = tempdir.listFiles();
+            for (File file : files) {
+                if (!file.delete()) {
+                    System.out.println("Failed to delete " + file);
+                }
+            }
+        }
+        return (tempdir.isDirectory());
+    }
+
+    public class signature extends View {
+        private static final float STROKE_WIDTH = 5f;
+        private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+        private Paint paint = new Paint();
+        private Path path = new Path();
+
+        private float lastTouchX;
+        private float lastTouchY;
+        private final RectF dirtyRect = new RectF();
+
+        public signature(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(STROKE_WIDTH);
+        }
+
+        public void save(View v) {
+            Log.v("log_tag", "Width: " + v.getWidth());
+            Log.v("log_tag", "Height: " + v.getHeight());
+            if (mBitmap == null) {
+                mBitmap = Bitmap.createBitmap(mContent.getWidth(), mContent.getHeight(), Bitmap.Config.RGB_565);
+                ;
+            }
+            Canvas canvas = new Canvas(mBitmap);
+            try {
+                final FileOutputStream mFileOutStream = new FileOutputStream(mypath);
+
+                v.draw(canvas);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mBitmap.compress(Bitmap.CompressFormat.PNG, 90, mFileOutStream);
+                    }
+                });
+                mFileOutStream.flush();
+                mFileOutStream.close();
+                String url = MediaStore.Images.Media.insertImage(getContentResolver(), mBitmap, "title", null);
+                Log.v("log_tag", "url: " + url);
+                //In case you want to delete the file
+//                boolean deleted = mypath.delete();
+                //Log.v("log_tag","deleted: " + mypath.toString() + deleted);
+                //If you want to convert the image to string use base64 converter
+
+            } catch (Exception e) {
+                Log.v("log_tag", e.toString());
+            }
+        }
+
+        public void clear() {
+            path.reset();
+            invalidate();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawPath(path, paint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float eventX = event.getX();
+            float eventY = event.getY();
+            mGetSign.setEnabled(true);
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    path.moveTo(eventX, eventY);
+                    lastTouchX = eventX;
+                    lastTouchY = eventY;
+                    return true;
+
+                case MotionEvent.ACTION_MOVE:
+
+                case MotionEvent.ACTION_UP:
+
+                    resetDirtyRect(eventX, eventY);
+                    int historySize = event.getHistorySize();
+                    for (int i = 0; i < historySize; i++) {
+                        float historicalX = event.getHistoricalX(i);
+                        float historicalY = event.getHistoricalY(i);
+                        expandDirtyRect(historicalX, historicalY);
+                        path.lineTo(historicalX, historicalY);
+                    }
+                    path.lineTo(eventX, eventY);
+                    break;
+
+                default:
+                    debug("Ignored touch event: " + event.toString());
+                    return false;
+            }
+
+            invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+                    (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+
+            lastTouchX = eventX;
+            lastTouchY = eventY;
+
+            return true;
+        }
+
+        private void debug(String string) {
+        }
+
+        private void expandDirtyRect(float historicalX, float historicalY) {
+            if (historicalX < dirtyRect.left) {
+                dirtyRect.left = historicalX;
+            } else if (historicalX > dirtyRect.right) {
+                dirtyRect.right = historicalX;
+            }
+
+            if (historicalY < dirtyRect.top) {
+                dirtyRect.top = historicalY;
+            } else if (historicalY > dirtyRect.bottom) {
+                dirtyRect.bottom = historicalY;
+            }
+        }
+
+        private void resetDirtyRect(float eventX, float eventY) {
+            dirtyRect.left = Math.min(lastTouchX, eventX);
+            dirtyRect.right = Math.max(lastTouchX, eventX);
+            dirtyRect.top = Math.min(lastTouchY, eventY);
+            dirtyRect.bottom = Math.max(lastTouchY, eventY);
+        }
+    }
+}
