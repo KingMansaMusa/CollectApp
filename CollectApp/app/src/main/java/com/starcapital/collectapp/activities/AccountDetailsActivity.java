bc69445eadccd17e4d889edda4b43b9699b655a8
Stephen Obeng Takyi <stephenta@stlghana.com>
2020-01-03 18:00:12 +0000

    More general additions in preparation for the Web integration

--- CollectApp/app/src/main/java/com/starcapital/collectapp/activities/AccountDetailsActivity.java
+++ CollectApp/app/src/main/java/com/starcapital/collectapp/activities/AccountDetailsActivity.java
@@ -1,11 +1,24 @@
 package com.starcapital.collectapp.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.starcapital.collectapp.R;
+import com.starcapital.collectapp.utilities.DialogUtility;
 
 public class AccountDetailsActivity extends AppCompatActivity {
+    ImageView imageViewAccPhoto, imageViewAccSignature;
+    EditText editTextAccountNumber, editTextAccountName, editTextContact, editTextIDNumber, editTextOfficer;
+    Spinner spinnerBranch, spinnerIDType;
+    Button buttonCancel, buttonEdit;
+    Context context;
+    DialogUtility dialogUtility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,36 @@ public class AccountDetailsActivity extends AppCompatActivity {
         getSupportActionBar().show();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        init();
+
+        final ArrayAdapter<String> idTypeAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.id_types));
+        spinnerIDType.setAdapter(idTypeAdapter);
+
+        final ArrayAdapter<String> branchAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.branches));
+        spinnerBranch.setAdapter(branchAdapter);
     }
+
+    private void init(){
+        context = AccountDetailsActivity.this;
+        dialogUtility = new DialogUtility(context);
+        imageViewAccPhoto = findViewById(R.id.account_details_account_photo);
+        imageViewAccSignature = findViewById(R.id.account_details_account_signature);
+        editTextAccountNumber = findViewById(R.id.account_details_account_number);
+        editTextAccountName = findViewById(R.id.account_details_account_name);
+        editTextContact = findViewById(R.id.account_details_contact);
+        spinnerIDType = findViewById(R.id.account_details_id_type);
+        editTextIDNumber = findViewById(R.id.account_details_id_number);
+        editTextOfficer = findViewById(R.id.account_details_officer);
+        spinnerBranch = findViewById(R.id.account_details_branch);
+
+        buttonCancel = findViewById(R.id.account_details_cancel_button);
+        buttonEdit = findViewById(R.id.account_details_edit_button);
+
+    }
+
 }
